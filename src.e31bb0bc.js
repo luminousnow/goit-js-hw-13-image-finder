f64e02fe0d4af3ccb8142f05480bc1879db9fab4
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-15 11:16:58 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -3248,7 +3248,7 @@ function clearPageMarkup() {
 function scrollPage() {
   // window.scrollBy(0, -350);
   window.scrollTo({
-    top: document.body.scrollHeight,
+    top: document.body.scrollHeight - 100,
     behavior: 'smooth'
   });
 }
@@ -3280,7 +3280,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49792" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53162" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
