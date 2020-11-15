ade6952420e9ccd44133a9887ecd0fa9ee870f35
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-15 11:21:47 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -3280,7 +3280,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53162" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53321" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
